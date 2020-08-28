PASTAplus/DataPortal

commit d1f5e11313a7b5727ca5c7ef55a05526d49ec23c

Change localPath to fixed location with report name only as parameter

src/edu/lternet/pasta/portal/ReportViewerServlet.java
@@ -65,6 +65,8 @@ public class ReportViewerServlet extends DataPortalServlet {
 
   private static final String HTMLTAIL = "    </div>\n</body>\n</html>\n";
 
+  private static final String REPORTJAIL = "/home/pasta/local/report_jail/";
+
 	  /**
    * Constructor of the object.
    */
@@ -116,8 +118,9 @@ public class ReportViewerServlet extends DataPortalServlet {
 			uid = "public";
 
 		String packageId = request.getParameter("packageid");
-		String encodedPath = request.getParameter("localPath");
+		String encodedName = request.getParameter("reportName");
 		String transactionId = request.getParameter("transactionId");
+
 		String scope = null;
 		Integer identifier = null;
 		String revision = null;
@@ -125,13 +128,6 @@ public class ReportViewerServlet extends DataPortalServlet {
 		String html = null;
 
 		try {
-			
-			String[] tokens = packageId.split("\\.");
-
-			if (tokens.length == 3) {
-				scope = tokens[0];
-				identifier = Integer.valueOf(tokens[1]);
-				revision = tokens[2];
 			
 				/*
 				 * The quality report XML could be read either from a local file
@@ -143,31 +139,37 @@ public class ReportViewerServlet extends DataPortalServlet {
 					xml = dpmClient.readEvaluateReport(scope, identifier, revision, transactionId);
 					
 				}				
-				else if (encodedPath != null && encodedPath.length() > 0) {
+				else if (encodedName != null && encodedName.length() > 0) {
 					URLCodec urlCodec = new URLCodec();
-					String localPath = urlCodec.decode(encodedPath);
-					File xmlFile = new File(localPath);
-					if (xmlFile != null && xmlFile.exists()) {
+					String reportName = urlCodec.decode(encodedName);
+					File xmlFile = new File(REPORTJAIL + reportName);
+					if (xmlFile.exists()) {
 						xml = FileUtils.readFileToString(xmlFile);
 					}
 				}
 				else {
-					String userAgent = request.getHeader("User-Agent");
-					DataPackageManagerClient dpmClient = new DataPackageManagerClient(uid, userAgent);
-					xml = dpmClient.readDataPackageReport(scope, identifier, revision);
+					String[] tokens = packageId.split("\\.");
+					if (tokens.length == 3) {
+						scope = tokens[0];
+						identifier = Integer.valueOf(tokens[1]);
+						revision = tokens[2];
+
+						String userAgent = request.getHeader("User-Agent");
+						DataPackageManagerClient dpmClient = new DataPackageManagerClient(uid, userAgent);
+						xml = dpmClient.readDataPackageReport(scope, identifier, revision);
+					}
+					else {
+						String msg = String
+								.format("packageId '%s' is not in the correct form of 'scope.identifier.revision' (e.g., 'knb-lter-lno.1.1')",
+										packageId);
+						throw new UserErrorException(msg);
+					}
 				}
 
 				ReportUtility qrUtility = new ReportUtility(xml);
 				html = HTMLHEAD + "<div class=\"qualityreport\">"
 						+ qrUtility.xmlToHtmlTable(cwd + xslpath) + "</div>"
 						+ HTMLTAIL;
-			}
-			else {
-				String msg = String
-						.format("packageId '%s' is not in the correct form of 'scope.identifier.revision' (e.g., 'knb-lter-lno.1.1')",
-								packageId);
-				throw new UserErrorException(msg);
-			}
 		}
 		catch (Exception e) {
 			handleDataPortalError(logger, e);

commit 0b63fb50853efdc1152e5701d73630b12737a698

Correct regression from (d1f5e11)

src/edu/lternet/pasta/portal/ReportViewerServlet.java
@@ -118,7 +118,7 @@ public class ReportViewerServlet extends DataPortalServlet {
 			uid = "public";
 
 		String packageId = request.getParameter("packageid");
-		String encodedName = request.getParameter("reportName");
+		String encodedName = request.getParameter("localPath");
 		String transactionId = request.getParameter("transactionId");
 
 		String scope = null;
@@ -142,7 +142,7 @@ public class ReportViewerServlet extends DataPortalServlet {
 				else if (encodedName != null && encodedName.length() > 0) {
 					URLCodec urlCodec = new URLCodec();
 					String reportName = urlCodec.decode(encodedName);
-					File xmlFile = new File(REPORTJAIL + reportName);
+					File xmlFile = new File(reportName);
 					if (xmlFile.exists()) {
 						xml = FileUtils.readFileToString(xmlFile);
 					}