8fa3fd75b4779ba1277f4bc32652287842dbc9fc
rogerdahl <git@dahlsys.com>
2022-08-31 01:02:28 -0600

    Ridare integration and line based more/less

--- WebRoot/js/more.js
+++ WebRoot/js/more.js
@@ -1,53 +1,40 @@
-//     <div class="ridare-text">
-//       <div class="ridare-content">
-//         {}
-//       </div>
-//       <div class="ridare-more">
-//       </div>
-//     </div>
+// Structure of HTML fragment returned from Ridare:
+//
+// <div class="ridare-text">
+//   <div class="ridare-content">
+//     { HTML }
+//   </div>
+//   <div class="ridare-more">
+//   </div>
+// </div>
 
-const MAX_HEIGHT = 30;
-
-function isOverflowed(content_el)
+function toggle_clamp(ev)
 {
-  // return content_el.scrollHeight > content_el.clientHeight;
-  return content_el.clientHeight > MAX_HEIGHT;
-}
-
-function toggle_clamp(text_el)
-{
-  const content_el = text_el.getElementsByClassName("ridare-content")[0];
-  const more_el = text_el.getElementsByClassName("ridare-more")[0];
-  if (content_el.classList.contains("ridare-clamp")) {
-    content_el.classList.remove("ridare-clamp");
+  const more_el = ev.target;
+  const text_el = more_el.parentElement;
+  const content_el = text_el.getElementsByTagName("div")[0];
+  if (content_el.classList.contains("ridare-content")) {
+    content_el.classList.remove("ridare-content");
+    content_el.classList.add("ridare-content-all");
     more_el.classList.remove("ridare-more-show");
     more_el.classList.add("ridare-less-show");
   }
   else {
-    content_el.classList.add("ridare-clamp");
+    content_el.classList.add("ridare-content");
+    content_el.classList.remove("ridare-content-all");
     more_el.classList.add("ridare-more-show");
     more_el.classList.remove("ridare-less-show");
   }
 }
 
-function toggle_clamp_ev(ev)
-{
-  const more_el = ev.target;
-  const text_el = more_el.parentElement;
-  toggle_clamp(text_el);
-}
-
 for (const text_el of document.getElementsByClassName("ridare-text")) {
   const content_el = text_el.getElementsByClassName("ridare-content")[0];
-  if (isOverflowed(content_el)) {
-    toggle_clamp(text_el);
+  if (content_el.scrollHeight > content_el.clientHeight) {
+    const more_el = text_el.getElementsByClassName("ridare-more")[0];
+    more_el.classList.add("ridare-more-show");
   }
-  // else {
-  // }
-  // more_el.style.display = "block";
-  // more_el.style.display = "none";
 }
 
 for (const el of document.getElementsByClassName("ridare-more")) {
-  el.addEventListener("click", toggle_clamp_ev);
+  el.addEventListener("click", toggle_clamp);
 }
