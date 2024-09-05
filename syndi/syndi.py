6f3d6f29badd9149a2d8739e9e9c5fb4a5ed83eb
rogerdahl <git@dahlsys.com>
2024-09-05 08:22:04 -0600

    Change Syndi User-Agent to make requests from Syndi easier to recognize #146

--- syndi/syndi.py
+++ syndi/syndi.py
@@ -29,7 +29,7 @@ def is_service_up(service_url):
             service_url,
             timeout=5,
             headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+                'User-Agent': 'PASTA Syndi monitoring service'
             },
         )
         # print(response.text)
