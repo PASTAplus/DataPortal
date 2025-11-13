bc765a7c2e04b4a500e8e7dfa08e99d3ff184759
Mark Servilla <mark.servilla@gmail.com>
2025-11-13 21:59:16 +0000

    Modify edi_token column to have unlimited length

--- WebRoot/WEB-INF/conf/create_schemas.sql
+++ WebRoot/WEB-INF/conf/create_schemas.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA authtoken AUTHORIZATION pasta;
 CREATE TABLE authtoken.tokenstore (
   user_id VARCHAR(248) NOT NULL,          -- user id, the primary key
   token VARCHAR(1024) NOT NULL,           -- base64 encoded auth token
-  edi_token VARCHAR(1024) NOT NULL,       -- base64 encoded EDI token
+  edi_token TEXT NOT NULL,       -- base64 encoded EDI token
   date_created TIMESTAMP DEFAULT now(),   -- insertion/update date/time
   CONSTRAINT token_store_pk PRIMARY KEY (user_id)
 );
